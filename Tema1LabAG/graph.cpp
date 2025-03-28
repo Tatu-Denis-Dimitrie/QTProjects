09ef3ce7d1c89d96b801df29828e5e5658f76a18
Tatu Denis <denis.tatu@student.unitbv.ro>
2025-03-28 22:14:55 +0200

    1st Project Upload

---
+++ Tema1LabAG/graph.cpp
@@ -0,0 +1,89 @@
+#include "Graph.h"
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
+#include <cmath>
+
+Graph::Graph() : oriented(true) {}
+
+Graph::~Graph() {}
+
+QVector<Node>& Graph::getNodes() {
+    return nodes;
+}
+
+QVector<Edge>& Graph::getEdges() {
+    return edges;
+}
+
+bool Graph::isOriented() const {
+    return oriented;
+}
+
+void Graph::addNode(QPoint pos) {
+    nodes.append(Node(pos, nodes.size()));
+    saveAdjacencyMatrix();
+}
+
+void Graph::addEdge(int first, int second) {
+    Edge newEdge(first, second);
+    if (!isDuplicateEdge(newEdge)) {
+        edges.append(newEdge);
+        if (!oriented) {
+            edges.append(Edge(second, first));
+        }
+        saveAdjacencyMatrix();
+    } else {
+        QMessageBox::warning(nullptr, "Eroare", "Exista deja acest arc.");
+    }
+}
+
+void Graph::setGraphOrientation(bool oriented) {
+    this->oriented = oriented;
+    saveAdjacencyMatrix();
+}
+
+bool Graph::isDuplicateEdge(const Edge& newEdge) {
+    for (const auto& edge : edges) {
+        if (edge.getFirst() == newEdge.getFirst() && edge.getSecond() == newEdge.getSecond()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void Graph::saveAdjacencyMatrix() {
+    int numNodes = nodes.size();
+    QVector<QVector<int>> adjacencyMatrix(numNodes, QVector<int>(numNodes, 0));
+
+    for (const auto& edge : edges) {
+        adjacencyMatrix[edge.getFirst()][edge.getSecond()] = 1;
+        if (!oriented) {
+            adjacencyMatrix[edge.getSecond()][edge.getFirst()] = 1;
+        }
+    }
+
+    QFile file("matrice_adiacenta.txt");
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        QTextStream out(&file);
+        out << numNodes << "\n";
+        for (int i = 0; i < numNodes; ++i) {
+            for (int j = 0; j < numNodes; ++j) {
+                out << adjacencyMatrix[i][j] << " ";
+            }
+            out << "\n";
+        }
+        file.close();
+    }
+}
+
+int Graph::getNodeAtPosition(const QPoint &pos) const {
+    for (int i = 0; i < nodes.size(); ++i) {
+        int dx = nodes[i].getPos().x() - pos.x();
+        int dy = nodes[i].getPos().y() - pos.y();
+        if (std::sqrt(dx * dx + dy * dy) <= 10) {
+            return i;
+        }
+    }
+    return -1;
+}
---
+++ Tema1LabAG/graph.h
@@ -0,0 +1,29 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <QVector>
+#include "node.h"
+#include "edge.h"
+
+class Graph {
+public:
+    Graph();
+    ~Graph();
+
+    void addNode(QPoint pos);
+    void addEdge(int first, int second);
+    void setGraphOrientation(bool oriented);
+    bool isDuplicateEdge(const Edge& newEdge);
+    void saveAdjacencyMatrix();
+    int getNodeAtPosition(const QPoint &pos) const;
+    QVector<Node>& getNodes();
+    QVector<Edge>& getEdges();
+    bool isOriented() const;
+
+private:
+    QVector<Node> nodes;
+    QVector<Edge> edges;
+    bool oriented;
+};
+
+#endif // GRAPH_H
